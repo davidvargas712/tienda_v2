ac0388944e1c4d8ebf3e4e4d7d8574961631fb95
Usuario <Usuario@DESKTOP-FSQ9PDR>
2023-06-27 17:19:48 -0600

    semana#6

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -14,5 +14,13 @@ public interface CategoriaService {
     
     
     //abajo se colocaran los metodos para un CRUD de categorias... pero en semana 6
+    // Se obtiene un Categoria, a partir del id de un categoria
+    public Categoria getCategoria(Categoria categoria);
     
+    // Se inserta un nuevo categoria si el id del categoria esta vacío
+    // Se actualiza un categoria si el id del categoria NO esta vacío
+    public void save(Categoria categoria);
+    
+    // Se elimina el categoria que tiene el id pasado por parámetro
+    public void delete(Categoria categoria);
 }
